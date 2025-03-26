9633e2091fe0ae832f90d026e75c2f1d344f1fe6
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-03-26 12:41:25 +0530

    Add test for ExpenseDelete mutation to verify successful expense deletion

---
+++ spec/graphql/mutations/expense_delete_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'ExpenseDelete Mutation', type: :request do
+  describe 'deleting an expense' do
+    let(:expense) { create(:expense) }
+
+    let(:mutation) do
+      <<~GQL
+        mutation($id: ID!) {
+          expenseDelete(id: $id) {
+            message
+            errors
+          }
+        }
+      GQL
+    end
+
+    let(:params) do
+      {
+        id: expense.id
+      }
+    end
+
+    context 'when the mutation is successful' do
+      it 'deletes an expense and returns a success message' do
+        post '/graphql', params: { query: mutation, variables: params.to_json }, headers: { 'Content-Type': 'application/json' }
+
+        json = JSON.parse(response.body)
+        data = json['data']['expenseDelete']
+
+        expect(data).to include(
+          'message' => 'Expense deleted successfully',
+          'errors' => []
+        )
+      end
+    end
+  end
+end
